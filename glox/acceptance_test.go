094ab8188ba0e6965bdaa4460cb2dcdc8a2169ed
Ryan Brushett <ryanbrushett@gmail.com>
2022-09-12 22:55:35 -0400

    Disable acceptance tests

--- glox/acceptance_test.go
+++ glox/acceptance_test.go
@@ -24,7 +24,7 @@ func TestAcceptanceTests(t *testing.T) {
 	chapters := map[string]string{
 		"chap04_scanning":    "false",
 		"chap06_parsing":     "false",
-		"chap07_evaluating":  "run",
+		"chap07_evaluating":  "false",
 		"chap08_statements":  "todo",
 		"chap09_control":     "todo",
 		"chap10_functions":   "todo",
